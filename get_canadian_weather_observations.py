7e72674b90d64a19698719e9c08811de3f0775be
Miguel Tremblay <gitlab.miguel@ptaff.ca>
2017-08-20 14:25:50 -0400

    Version 0.4

--- get_canadian_weather_observations.py
+++ get_canadian_weather_observations.py
@@ -45,7 +45,7 @@ from dateutil import rrule
 # From progress https://pypi.python.org/pypi/progress
 from progress.bar import Bar
 
-VERSION = "0.3"
+VERSION = "0.4"
 # Verbose level:
 ## 1 Normal mode
 ## 2 Full debug
